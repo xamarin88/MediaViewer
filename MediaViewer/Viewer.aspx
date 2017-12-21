fc02e75db4728e58498f459b6cbd526e70b8a5bc
Sharon <tai_siewyee@hotmail.com>
2017-12-21 21:06:19 +0800

    Add Solution to Repo
    Add Solution to Repo

---
+++ MediaViewer/Viewer.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Viewer.aspx.cs" Inherits="MediaViewer.Viewer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="frmMediaViewer" runat="server">
+        <!-- Image viewer -->
+        <div>
+            <b>Please select your image here:</b>
+            <hr />
+            <asp:DropDownList ID="ddlImages" runat="server" AppendDataBoundItems="true" AutoPostBack="true" OnSelectedIndexChanged="FetchImage">
+                <asp:ListItem Text="Select Image" Value="0" />
+            </asp:DropDownList>
+            <hr />
+            <asp:Image ID="imgView" runat="server" Height="393px" Visible="false" Width="490px" />
+        </div>
+        <!-- Video viewer -->
+        <div>
+            <b>Please select your video here:</b>
+            <hr />
+            <asp:DropDownList ID="ddlVideos" runat="server" AppendDataBoundItems="true" AutoPostBack="true" OnSelectedIndexChanged="FetchVideo">
+                <asp:ListItem Text="Select Video" Value="0" />
+            </asp:DropDownList>
+            <hr />
+            <video width="400" controls>
+                <source type="video/mp4" id="vdView" runat="server" visible="false" Height="300px" Width="400px" />
+            </video>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MediaViewer/Viewer.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace MediaViewer
+{
+    public partial class Viewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                ddlImages.DataSource = GetData("SELECT imageId, filePath as filename FROM BlobStorage WHERE fileExt = 'jpg'");
+                ddlImages.DataTextField = "filename";
+                ddlImages.DataValueField = "filename";
+                ddlImages.DataBind();
+
+                ddlVideos.DataSource = GetData("SELECT imageId, filePath as filename FROM BlobStorage WHERE fileExt = 'mp4'");
+                ddlVideos.DataTextField = "filename";
+                ddlVideos.DataValueField = "filename";
+                ddlVideos.DataBind();
+            }
+        }
+
+        private DataTable GetData(string query)
+        {
+            DataTable dt = new DataTable();
+            
+            string constr = ConfigurationManager.AppSettings["ConnectionString"];
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Connection = con;
+                        cmd.CommandTimeout = 1000;
+                        sda.SelectCommand = cmd;
+                        sda.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+        }
+
+        protected void FetchImage(object sender, EventArgs e)
+        {
+            string path = ddlImages.SelectedItem.Value;
+            if (path != null)
+            {
+                imgView.Visible = true;
+                string filename = path.Split('/').Last();
+                CloudBlockBlob blob = GetBlobInContainer("images", filename);
+                imgView.ImageUrl = blob.Uri.AbsoluteUri.ToString();
+            }
+        }
+
+        CloudBlockBlob GetBlobInContainer(string container, string fileName)
+        {
+            //use web.config appSetting to get connection setting .NET Framework's ConfigurationManager class can also be used for this
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["BlobStorageConnectionString"]);
+            //create the blob client
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            //retrieve a refernce to a container
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference(ConfigurationManager.AppSettings["ImagesContainer"]);
+            //set permission to show to public
+            blobContainer.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            blobContainer.CreateIfNotExists();
+            // Retrieve reference to a blob named "photo1.jpg".
+            CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(fileName);
+            blockBlob.Properties.ContentType = "image/jpg";
+            return blockBlob;
+        }
+
+        protected void FetchVideo(object sender, EventArgs e)
+        {
+            string path = ddlVideos.SelectedItem.Value;
+            if (path != null)
+            {
+                vdView.Visible = true;
+                string filename = path.Split('/').Last();
+                CloudBlockBlob blob = GetBlobInContainer("images", filename);
+                vdView.Src = blob.Uri.AbsoluteUri.ToString();
+            }
+        }
+    }
+}
